befd7b39cbd2cd771dc8909455a2b313fb3cb0e2
RavinduTharanga <ravindutharangaperera@gmail.com>
2025-03-08 00:07:18 -0500

    commit

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,32 @@
-import { Col } from "react-bootstrap";
+// import { Col } from "react-bootstrap";
+
+// export const ProjectCard = ({ title, description, imgUrl }) => {
+//   return (
+//     <Col size={12} sm={6} md={4}>
+//       <div className="proj-imgbx">
+//         <img className="card" src={imgUrl} />
+//         <div className="proj-txtx">
+//           <h4>{title}</h4>
+//           <span>{description}</span>
+//         </div>
+//       </div>
+//     </Col>
+//   )
+// }
+import React from 'react';
+import { CCard, CCardBody, CCardImage, CCardTitle, CCardText, CButton } from '@coreui/react';
 
 export const ProjectCard = ({ title, description, imgUrl }) => {
   return (
-    <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img className="card" src={imgUrl} />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
-        </div>
-      </div>
-    </Col>
-  )
-}
+    <CCard style={{ width: '100%', marginBottom: '20px' }}>
+      <CCardImage orientation="top" src={imgUrl} />
+      <CCardBody>
+        <CCardTitle>{title}</CCardTitle>
+        <CCardText>{description}</CCardText>
+        <CButton color="primary" href="#">
+          Learn More
+        </CButton>
+      </CCardBody>
+    </CCard>
+  );
+};
